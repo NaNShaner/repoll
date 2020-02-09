792602088f8c58df7fd076a10a7a8b69d6e26305
bijingrui <bijingrui@126.com>
2020-02-09 16:30:57 +0800

    新增接口路由

--- polls/urls.py
+++ polls/urls.py
@@ -5,13 +5,13 @@
 from django.urls import path
 from . import views
 from django.conf.urls import url, include
-from polls.apis import redisstop, redisstart, allredisins
-
+from polls.apis import redisstop, redisstart, allredisins, memory_action
 
 app_name = 'polls'
 urlpatterns = [
     path('redis_qps/<str:redis_type>/<str:ins_id>/<str:redis_ip>/<str:redis_port>/', views.redis_qps, name='redis_qps'),
     path(r'apis/redis-stop/<str:redis_type>/<str:ins_id>/', redisstop),
     path(r'apis/redis-start/<str:redis_type>/<str:ins_id>/', redisstart),
-    path(r'apis/redis-ins/<str:redis_type>/', allredisins)
+    path(r'apis/redis-ins/<str:redis_type>/', allredisins),
+    path(r'apis/redis-ins/<str:redis_type>/<str:redis_ins_name>/<str:memory>', memory_action)
 ]
