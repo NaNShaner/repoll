ecef140e1f5758f08adfad8769d9d274aae84040
bijingrui <bijingrui@126.com>
2020-03-08 16:32:10 +0800

    新增表单视图

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,13 @@
 from __future__ import unicode_literals
 from .models import RealTimeQps, RunningInsSentinel, RunningInsStandalone, RunningInsCluster
 import os
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+
+from .forms import NameForm
+
 # pyecharts
 from jinja2 import Environment, FileSystemLoader
 from pyecharts.globals import CurrentConfig
@@ -34,8 +41,6 @@ def redis_qps(request, redis_type, ins_id, redis_ip, redis_port):
     real_time_obj = real_time_qps.filter(redis_running_monitor_id=ins_id, redis_ip=redis_ip, redis_port=redis_port).order_by('-collect_date')[:60]
     running_ins = running_ins_time.filter(redis_ip=redis_ip, running_ins_port=redis_port)
     running_ins_name = running_ins.values('running_ins_name').first()
-    # running_ins_ip = running_ins.values('redis_ip').first()
-    # running_ins_port = running_ins.values('running_ins_port').first()
     real_time = [real_time.__dict__['collect_date'] for real_time in real_time_obj]
     redis_qps = [redis_qps.__dict__['redis_qps'] for redis_qps in real_time_obj]
     c = (
@@ -49,6 +54,18 @@ def redis_qps(request, redis_type, ins_id, redis_ip, redis_port):
                          datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],)
     )
     return HttpResponse(c.render_embed())
+
+
+@api_view(['GET'])
+@permission_classes((permissions.IsAuthenticated,))
+def get_ext_ins(request):
+    """
+    处理已导入redis实例表单页面
+    :param request:
+    :return:
+    """
+    form = NameForm()
+    return render(request, 'import_ext_ins.html', {'form': form})
 
 
 def favicon(request):
