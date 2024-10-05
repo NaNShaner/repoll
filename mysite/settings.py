ffd1baa6c1dce3a065ce7f6c1c26f0ca35e43914
bijingrui <bijingrui@126.com>
2024-10-05 22:53:09 +0800

    默认db改为sqlite3

--- mysite/settings.py
+++ mysite/settings.py
@@ -96,17 +96,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django',
+#         'USER': 'root',
+#         'PASSWORD': 'Pass@word',
+#         'HOST': '127.0.0.1',
+#         'PORT': '33306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': 'Pass@word',
-        'HOST': '127.0.0.1',
-        'PORT': '32768',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
-
 
 CACHES = {
     'default': {
